0b7397c2978995af55cc55af06610c71273d724a
ricky <rikanthanricky@gmail.com>
2020-04-19 10:21:23 +0530

    game over text

--- lib/finish.dart
+++ lib/finish.dart
@@ -1,121 +1,148 @@
-class position
+class finish
 {
-   int check01(int s)
+  int possible01(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i-1 >=0 && j-2 >=0)
+    if(arr.contains(s)==true && i-1 >=0 && j-2 >=0)
     {
       //System.out.println("positions are"+(i-1)+","+(j-2));
-       m=(i-1)*8+(j-2);
-      //System.out.println( "positions are "+m);
+      m=(i-1)*8+(j-2);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
+     
 
     }
-   /* check02(s);
-    check03(s);
-    check04(s);
-    check05(s);
-    check06(s);
-    check07(s);
-    check08(s);*/
+   
     return m;
   }
-  int check02(int s)
+  int possible02(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i-2 >=0 && j-1 >=0)
+    if(arr.contains(s)==true && i-2 >=0 && j-1 >=0)
     {
-       m=(i-2)*8+(j-1);
+      m=(i-2)*8+(j-1);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
       //System.out.println("positions are"+(i-2)+","+(j-1));
 
     }
     return m;
   }
-   int check03(int s)
+  int possible03(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i-1 >=0 && j+2 <=7)
+    if(arr.contains(s)==true && i-1 >=0 && j+2 <=7)
     {
       m=(i-1)*8+(j+2);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
       //System.out.println("positions are"+(i-1)+","+(j+2));
 
     }
     return m;
   }
-  int check04(int s)
+  int possible04(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i-2 >=0 && j+1 <=7)
+    if(arr.contains(s)==true && i-2 >=0 && j+1 <=7)
     {
       m=(i-2)*8+(j+1);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
 
       //System.out.println("positions are"+(i-2)+","+(j+1));
     }
     return m;
   }
 
-  int check05(int s)
+  int possible05(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i+1 <=7 && j-2 >=0)
+    if(arr.contains(s)==true && i+1 <=7 && j-2 >=0)
     {
       m=(i+1)*8+(j-2);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
 
       //System.out.println("positions are"+(i+1)+","+(j-2));
     }
     return m;
   }
-  int check06(int s)
+  int possible06(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i+1 <=7 && j+2 <=7)
+    if(arr.contains(s)==true && i+1 <=7 && j+2 <=7)
     {
       m=(i+1)*8+(j+2);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
 
       //System.out.println("positions are"+(i+1)+","+(j+2));
     }
     return m;
   }
-  int check07(int s)
+  int possible07(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i+2 <=7 && j-1 >=0)
+    if(arr.contains(s)==true && i+2 <=7 && j-1 >=0)
     {
-       m=(i+2)*8+(j-1);
+      m=(i+2)*8+(j-1);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
 
       //System.out.println("positions are"+(i+2)+","+(j-1));
     }
     return m;
+
   }
-  int check08(int s)
+  int possible08(int s,List arr)
   {
     int j=s%8;
     double i1=s/8;
     int  i=i1.toInt();
     int m=-1;
-    if( i+2 <=7 && j+1 <=7)
+    if(arr.contains(s)==true && i+2 <=7 && j+1 <=7)
     {
-       m=(i+2)*8+(j+1);
+      m=(i+2)*8+(j+1);
+      if(arr.contains(m)==true)
+      {
+        m=-1;
+      }
 
       //System.out.println("positions are"+(i+2)+","+(j+1));
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:horse_rider/finish.dart';
 import 'position.dart';
 import 'method.dart';
+
 
 const kopencolor=Colors.blueGrey;
 const kclickcolor=Colors.greenAccent;
@@ -16,21 +18,36 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
 
+
   int value1, value2, value3, value4, value5, value6, value7, value8, value9;
   int count = 0;
-
+  String s='';
   var arr = new List();
   Color curcolor;
+
   Color updatecolor(int index,List arr,int count)
   {
+    bool a=false;
+    bool b=false;
+    bool c=false;
 
-    if(arr.contains(index)==false && value9==index || arr.contains(value1)==false && value1==index || arr.contains(value2)==false && value2==index ||arr.contains(value3)==false && value3==index ||arr.contains(value3)==false && value4==index ||arr.contains(value5)==false && value5==index || arr.contains(value6)==false && value6==index || arr.contains(value7)==false && value7==index ||arr.contains(value8)==false&& value8==index )
+    if(arr.contains(index)==false && value9==index || arr.contains(value1)==false && value1==index || arr.contains(value2)==false && value2==index ||arr.contains(value3)==false && value3==index ||arr.contains(value4)==false && value4==index ||arr.contains(value5)==false && value5==index || arr.contains(value6)==false && value6==index || arr.contains(value7)==false && value7==index ||arr.contains(value8)==false&& value8==index )
 
     {
+      a=true;
+      //print('a:');
+     // print(a);
       return kclickcolor;
+
+
     }
     else if(arr.contains(index) )
     {
+      b=true;
+      a=false;
+      //print('b:');
+     // print(b);
+
       for(var i=0;i<count;i++){
         return klastcolor;
       }
@@ -39,9 +56,24 @@ class _MyAppState extends State<MyApp> {
 
     else
     {
+      c=true;
+      a=false;
+    //  print('c:');
+      //print(c);
       return kopencolor;
     }
+//    print(value1);
+//    print(value2);
+//    print(value3);
+//    print(value4);
+//    print(value5);
+//    print(value6);
+//    print(value7);
+//    print(value8);
+
+
   }
+
 
 
 
@@ -67,7 +99,8 @@ class _MyAppState extends State<MyApp> {
       count++;
     }
 
-    else if(count>0 && index==value1 || value2==index || value3==index || value4==index || value5==index || value6==index || value7==index || value8==index) {
+    else if(count>0 && arr.contains(value1)==false && value1==index || arr.contains(value2)==false && value2==index ||arr.contains(value3)==false && value3==index ||arr.contains(value4)==false && value4==index ||arr.contains(value5)==false && value5==index || arr.contains(value6)==false && value6==index || arr.contains(value7)==false && value7==index ||arr.contains(value8)==false&& value8==index )
+    {
       value1 = p.check01(index);
       value2 = p.check02(index);
       value3 = p.check03(index);
@@ -76,20 +109,67 @@ class _MyAppState extends State<MyApp> {
       value6 = p.check06(index);
       value7 = p.check07(index);
       value8 = p.check08(index);
+
       value9=index;
       arr.add(value9);
       count++;
+//      print(value1);
+//      print(value2);
+//      print(value3);
+//      print(value4);
+//      print(value5);
+//      print(value6);
+//      print(value7);
+//      print(value8);
+
+      if(arr.contains(value1)==true && arr.contains(value2)==true && arr.contains(value3)==true && arr.contains(value4)==true && arr.contains(value5)==true && arr.contains(value6)==true )//&& arr.contains(value7)==true && arr.contains(value8)==true  )
+        {
+          //gameover();
+          s=" game over";
+        }
       print(arr);
       print(count);
 
 
+
       //index == -1;
     }
+
+
+  }
+
+  void gameovercheck(int  index,List arr)
+  {
+    int g1=f.possible01(index, arr);
+    int g2=f.possible02(index, arr);
+    int g3=f.possible03(index, arr);
+    int g4=f.possible04(index, arr);
+    int g5=f.possible05(index, arr);
+    int g6=f.possible06(index, arr);
+    int g7=f.possible07(index, arr);
+    int g8=f.possible08(index, arr);
+
+    if(g1==-1 && g2==-1 && g3==-1 &&  g4==-1 && g5==-1 && g6==-1 && g7==-1 && g8==-1)
+      {
+    s=" Game over";
+
+      }
+   // s=" ";
+
+
   }
 
 
 
+
+
+
+
+
+
+
   position p = position();
+  finish f=finish();
   color_method c=color_method();
 
 
@@ -100,16 +180,17 @@ class _MyAppState extends State<MyApp> {
       title: 'Horse rider',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Horse rider'),
+          title: Text('Moves ' +count.toString()+s),
         ),
+
         body: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
+
           crossAxisCount: 8,
-          // Generate 100 widgets that display their index in the List.
+
           children: List.generate(64, (index) {
             return Container(
-              //color: Colors.blue,
+
+
               decoration: BoxDecoration(
                 border: Border.all(
                   color: Colors.black,
@@ -117,9 +198,6 @@ class _MyAppState extends State<MyApp> {
               ),
 
               child: RaisedButton(
-               //child: RaisedButton.icon(onPressed: null, icon: null, label: null),
-
-
 
                 onPressed: ()
                 {
@@ -128,11 +206,10 @@ class _MyAppState extends State<MyApp> {
                   check(index);
 
 
+                    });
+                  gameovercheck(index, arr);
 
-
-                  });
-
-                },
+                  },
 
 
 
@@ -146,6 +223,7 @@ class _MyAppState extends State<MyApp> {
             );
           }),
         ),
+
 
 
       ),
